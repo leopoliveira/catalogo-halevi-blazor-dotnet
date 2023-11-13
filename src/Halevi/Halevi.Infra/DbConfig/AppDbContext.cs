1feff9d196a004c20174f8e711eab8cbf7bcf662
Leonardo Oliveira <poliveira.leonardo@gmail.com>
2023-11-12 22:27:46 -0300

    Feat: Configurations.

--- src/Halevi/Halevi.Infra/DbConfig/AppDbContext.cs
+++ src/Halevi/Halevi.Infra/DbConfig/AppDbContext.cs
@@ -1,6 +1,20 @@
-﻿namespace Halevi.Infra.DbConfig
+﻿using Halevi.Infra.DbConfig.Configurations;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Halevi.Infra.DbConfig
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+
+            modelBuilder.ApplyConfiguration(new ProductVariationConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
