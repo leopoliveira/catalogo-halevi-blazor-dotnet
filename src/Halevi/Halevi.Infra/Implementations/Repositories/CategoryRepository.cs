d7755ea154d896cfcd2f7aea7ef3d5482eb4d267
Leonardo Oliveira <poliveira.leonardo@gmail.com>
2023-11-19 16:50:51 -0300

    Feat: Adding GetAll without Pagination parameter.

--- src/Halevi/Halevi.Infra/Implementations/Repositories/CategoryRepository.cs
+++ src/Halevi/Halevi.Infra/Implementations/Repositories/CategoryRepository.cs
@@ -3,12 +3,29 @@ using Halevi.Core.Domain.Interfaces.Repositories;
 using Halevi.Infra.DbConfig;
 using Halevi.Infra.Implementations.Repositories.Base;
 
+using Microsoft.EntityFrameworkCore;
+
 namespace Halevi.Infra.Implementations.Repositories
 {
     public class CategoryRepository : BaseRepository<Category>, ICategoryRepository
     {
+        private readonly AppDbContext _context;
+
         public CategoryRepository(AppDbContext dbContext) : base(dbContext)
         {
+            _context = dbContext;
+        }
+
+        /// <summary>
+        /// Gets all available categories.
+        /// </summary>
+        /// <returns>All available categories.</returns>
+        public async Task<IEnumerable<Category>> GetAllAsync()
+        {
+            return await _context.CATEGORY
+                .AsNoTracking()
+                .Where(x => x.Active)
+                .ToListAsync();
         }
     }
 }
