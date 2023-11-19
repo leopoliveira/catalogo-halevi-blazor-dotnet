88e75602bf9291915415a67d75b9706e1db4f946
Leonardo Oliveira <poliveira.leonardo@gmail.com>
2023-11-19 16:41:06 -0300

    Refactor: Adding Summary.

--- src/Halevi/Halevi.API/Controllers/v1/Base/BaseController.cs
+++ src/Halevi/Halevi.API/Controllers/v1/Base/BaseController.cs
@@ -4,6 +4,9 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Halevi.API.Controllers.v1.Base
 {
+    /// <summary>
+    /// Base Controller Implementation.
+    /// </summary>
     [Route($"api/{ApiConstants.API_V1}/[controller]")]
     [ApiController]
     [Consumes("application/json")]
