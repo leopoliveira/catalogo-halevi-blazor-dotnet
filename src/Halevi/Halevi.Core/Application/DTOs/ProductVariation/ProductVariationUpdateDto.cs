d90b0a865cc4620058e9509ea0c95e2639741170
Leonardo Oliveira <poliveira.leonardo@gmail.com>
2023-11-21 15:02:41 -0300

    Feat: Dtos refactor.

---
+++ src/Halevi/Halevi.Core/Application/DTOs/ProductVariation/ProductVariationUpdateDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Halevi.Core.Application.DTOs.ProductVariation
+{
+    public record ProductVariationUpdateDto : ProductVariationCreateDto
+    {
+        public Guid Id { get; set; }
+    }
+}
