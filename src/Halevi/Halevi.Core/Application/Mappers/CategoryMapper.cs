f53b5863394a57ac9efe30e5ec78bb60d2bd4467
Leonardo Oliveira <poliveira.leonardo@gmail.com>
2023-11-21 15:03:19 -0300

    Refactor: Adapting to the new Dto system.

--- src/Halevi/Halevi.Core/Application/Mappers/CategoryMapper.cs
+++ src/Halevi/Halevi.Core/Application/Mappers/CategoryMapper.cs
@@ -5,23 +5,45 @@ namespace Halevi.Core.Application.Mappers
 {
     public static class CategoryMapper
     {
-        public static Category ToEntity(this CategoryDto dto)
+        public static Category ToEntity(this CategoryCreateDto dto)
         {
             return new Category()
             {
                 Name = dto.Name,
-                Code = dto.Code,
+                Code = dto.Code.Value,
                 Active = dto.Active
             };
         }
 
-        public static CategoryDto ToDto(this Category category)
+        public static Category ToEntity(this CategoryUpdateDto dto)
         {
-            return new CategoryDto()
+            return new Category()
+            {
+                Name = dto.Name,
+                Id = dto.Id,
+                Code = dto.Code.Value,
+                Active = dto.Active
+            };
+        }
+
+        public static CategoryReadDto ToReadDto(this Category category)
+        {
+            return new CategoryReadDto()
             {
                 Id = category.Id,
                 Code = category.Code,
                 Name = category.Name,
+                Active = category.Active
+            };
+        }
+
+        public static CategoryUpdateDto ToUpdateDto(this Category category)
+        {
+            return new CategoryUpdateDto()
+            {
+                Id = category.Id,
+                Name = category.Name,
+                Code = category.Code,
                 Active = category.Active
             };
         }
