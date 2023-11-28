7c456d6530c98cfb9d683aff5d10524482a9d2d4
Leonardo Oliveira <poliveira.leonardo@gmail.com>
2023-11-28 14:55:53 -0300

    Feat: Product Variation Validation Test.

---
+++ src/Halevi/Halevi.Tests/Unit/Validations/ProductVariationValidationTest.cs
@@ -0,0 +1,90 @@
+﻿using Halevi.Core.Application.Helpers;
+using Halevi.Core.Domain.Validations;
+
+namespace Halevi.Tests.Unit.Validations
+{
+    public class ProductVariationVariationValidationTest
+    {
+        private readonly ProductVariationValidations _validationRules = new();
+
+        [Fact]
+        // Method_Scenario_Result
+        public void ProductVariationValidation_ValidProductVariation_ValidationOk()
+        {
+            // Arrange
+            ProductVariation entity = EntityFactory.MakeVariation();
+
+            // Act
+            bool validationResult = _validationRules.Validate(entity).IsValid;
+
+            // Assert
+            validationResult
+                .Should()
+                .BeTrue();
+        }
+
+        [Fact]
+        public void ProductVariationValidation_EmptyProductVariationName_ValidationNotOk()
+        {
+            // Arrange
+            ProductVariation entity = EntityFactory.MakeVariation();
+
+            // Act
+            entity.Name = string.Empty;
+            bool validationResult = _validationRules.Validate(entity).IsValid;
+
+            // Assert
+            validationResult
+                .Should()
+                .BeFalse();
+        }
+
+        [Fact]
+        public void ProductVariationValidation_EmptyProductId_ValidationNotOk()
+        {
+            // Arrange
+            ProductVariation entity = EntityFactory.MakeVariation();
+
+            // Act
+            entity.ProductId = Guid.Empty;
+            bool validationResult = _validationRules.Validate(entity).IsValid;
+
+            // Assert
+            validationResult
+                .Should()
+                .BeFalse();
+        }
+
+        [Fact]
+        public void ProductVariationValidation_EmpyProductVariationCreatedAt_ValidationNotOk()
+        {
+            // Arrange
+            ProductVariation entity = EntityFactory.MakeVariation();
+
+            // Act
+            entity.CreatedAt = DateOnly.MinValue;
+            bool validationResult = _validationRules.Validate(entity).IsValid;
+
+            // Assert
+            validationResult
+                .Should()
+                .BeFalse();
+        }
+
+        [Fact]
+        public void ProductVariationValidation_InvalidProductVariationCode_ValidationNotOk()
+        {
+            // Arrange
+            ProductVariation entity = EntityFactory.MakeVariation();
+
+            // Act
+            entity.Code = 0;
+            bool validationResult = _validationRules.Validate(entity).IsValid;
+
+            // Assert
+            validationResult
+                .Should()
+                .BeFalse();
+        }
+    }
+}
