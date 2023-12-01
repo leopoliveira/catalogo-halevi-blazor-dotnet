bf8ea6aa2bb16533ae6c49b33dc2b9b680ba0b76
Leonardo Oliveira <poliveira.leonardo@gmail.com>
2023-11-30 22:15:17 -0300

    Feat: Initial structure.

--- src/Front/halevi.front/src/app/page.tsx
+++ src/Front/halevi.front/src/app/page.tsx
@@ -1,3 +1,23 @@
-export default function Home() {
-  return <div></div>;
+export default function Page() {
+  return (
+    <>
+      <div className="main">
+        <div id="nav-bar">
+          <div id="logo"></div>
+          <div id="go-to-cart"></div>
+          <div id="categories"></div>
+        </div>
+
+        <div id="main-content">
+          <div id="header">
+            <div id="search-bar"></div>
+            <div id="cart-icon"></div>
+          </div>
+          <div id="product-cards"></div>
+        </div>
+      </div>
+
+      <div id="footer"></div>
+    </>
+  );
 }
