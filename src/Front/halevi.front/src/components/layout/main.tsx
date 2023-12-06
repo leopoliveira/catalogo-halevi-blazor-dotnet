1718c9c66b4d4de990b7ffea4ad8564d2f387aa1
Leonardo Oliveira <poliveira.leonardo@gmail.com>
2023-12-05 23:52:19 -0300

    Refactor Remove unnecessary class from main element

--- src/Front/halevi.front/src/components/layout/main.tsx
+++ src/Front/halevi.front/src/components/layout/main.tsx
@@ -2,7 +2,7 @@ import ProductCard from "../product/product-card";
 
 export default function Main() {
   return (
-    <main className="ml-2 flex-1 text-center pr-4 pt-4 pb-4 z-30 shadow-sm rounded-md bg-slate-50">
+    <main className="text-center pr-4 pt-4 pb-4 z-30 shadow-sm rounded-md bg-slate-50">
       <div className="grid grid-cols-1 md:grid-cols-2 min-auto lg:grid-cols-3 min-auto gap-4">
         <ProductCard />
         <ProductCard />
