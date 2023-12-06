71ad8346eed4a86831befa5ba8f7db5a1fe15ce1
Leonardo Oliveira <poliveira.leonardo@gmail.com>
2023-12-05 23:31:56 -0300

    Feat: Add footer, header, and navbar components

---
+++ src/Front/halevi.front/src/components/layout/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bottom-0 w-100% text-center mt-2 flex flex-row items-center justify-between p-4 z-30 shadow-sm rounded-md bg-slate-50">
+      -
+    </footer>
+  );
+}
---
+++ src/Front/halevi.front/src/components/layout/header.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="flex flex-row items-center justify-between p-4 z-30 shadow-sm rounded-md bg-slate-50">
+      <Link
+        href={"/"}
+        id="logo">
+        Logo
+      </Link>
+
+      <Link
+        href={"/cart"}
+        id="go-to-cart">
+        Shopping Cart
+      </Link>
+    </header>
+  );
+}
