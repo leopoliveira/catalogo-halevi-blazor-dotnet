103e966fd9fd8531aa04578879fdc708b8e37386
Leonardo Oliveira <poliveira.leonardo@gmail.com>
2023-12-06 08:26:27 -0300

    Refactor: Product card component

--- src/Front/halevi.front/src/components/product/product-card.tsx
+++ src/Front/halevi.front/src/components/product/product-card.tsx
@@ -1,27 +1,25 @@
+import Image from "next/image";
+
 export default function ProductCard() {
   return (
     <>
-      <div className="flex flex-col bg-white shadow-lg rounded-lg p-6 m-4">
-        <div
-          className="w-1/2 h-32 bg-cover bg-center mb-4 mx-auto"
-          style={{
-            backgroundImage: "url('https://via.placeholder.com/75')",
-          }}></div>
-        <div
-          id="body"
-          className="flex flex-col h-32 justify-between">
-          <div className="font-bold text-xl mb-2">Product 1</div>
-          <div className="text-gray-700 mb-2">Category</div>
-          <div className="text-gray-900 font-bold text-lg mb-2">
-            R$ 200,00
-          </div>
+      <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+        <Image
+          src="https://via.placeholder.com/75"
+          alt="title"
+          width={500}
+          height={300}
+          objectFit="cover"
+        />
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">{"Product 1"}</div>
+          <div className="font-bold text-xl mb-2">{"Category"}</div>
+          <p className="text-gray-700 text-base">R$200,30</p>
         </div>
-        <div
-          id="footer"
-          className="flex items-center justify-center">
-          <div className="w-1/2 bg-green-700 text-white text-center py-2 rounded hover:bg-green-500 hover:text-black hover:font-bold transition-colors duration-300 cursor-pointer">
+        <div className="px-6 pt-4 pb-2">
+          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
             Add to Cart
-          </div>
+          </button>
         </div>
       </div>
     </>
