2b4efb2dbc6559948bcfade1972c588b916a7aa7
Leonardo Oliveira <poliveira.leonardo@gmail.com>
2023-12-05 23:32:15 -0300

    Feat: Add main layout component and product card component

---
+++ src/Front/halevi.front/src/components/layout/main.tsx
@@ -0,0 +1,9 @@
+import ProductCard from "../product/product-card";
+
+export default function Main() {
+  return (
+    <main className="ml-2 flex-1 text-center pr-4 pt-4 pb-4 z-30 shadow-sm rounded-md bg-slate-50">
+      <ProductCard />
+    </main>
+  );
+}
---
+++ src/Front/halevi.front/src/components/product/product-card.tsx
@@ -0,0 +1,29 @@
+export default function ProductCard() {
+  return (
+    <>
+      <div className="flex flex-col bg-white shadow-lg rounded-lg p-6 m-4 overflow-hidden w-1/2">
+        <div
+          className="w-1/2 h-48 bg-cover bg-center mb-4 mx-auto"
+          style={{
+            backgroundImage: "url('https://via.placeholder.com/75')",
+          }}></div>
+        <div
+          id="body"
+          className="flex flex-col justify-between flex-grow">
+          <div className="font-bold text-xl mb-2">Product 1</div>
+          <div className="text-gray-700 mb-2">Category</div>
+          <div className="text-gray-900 font-bold text-lg mb-2">
+            R$ 200,00
+          </div>
+        </div>
+        <div
+          id="footer"
+          className="flex items-center justify-center">
+          <div className="w-1/2 bg-green-700 text-white text-center py-2 rounded hover:bg-green-500 hover:text-black hover:font-bold transition-colors duration-300 cursor-pointer">
+            Add to Cart
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
